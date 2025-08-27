2bd80c62be8f712fd192cc0b23ffbbc343033d56
yamass <yamass@users.noreply.github.com>
2025-08-27 17:13:22 +0000

    deploy: f3b197337a7c97fb4fc482eec0f70a0afec39479

--- visualizer/sw.js
+++ visualizer/sw.js
@@ -46,7 +46,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "8/27/2025, 5:12:31 PM",
+  "version": "8/27/2025, 5:13:20 PM",
   "name": "webpack-offline",
   "pluginVersion": "4.9.1",
   "relativePaths": true
